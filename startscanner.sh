e6bd0426f2c30d46c246d132c0ca77c575dfe8ea
andreasmuthig <155832695+andreasmuthig@users.noreply.github.com>
2025-04-08 20:03:58 +0200

    Delete startscanner.sh

--- startscanner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")" || exit 1
-echo "📁 Ordner: $(pwd)"
-echo "🚀 Starte lokalen Webserver auf Port 8000..."
-python3 -m http.server 8000 &
-sleep 2
-echo "🌐 Öffne Browser..."
-open "http://localhost:8000/index.html"
